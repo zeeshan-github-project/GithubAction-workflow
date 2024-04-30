eaa5d59f46ef67af29fdbd7c52b99c60a25dc313
zeeshan-github-project <57130457+zeeshan-github-project@users.noreply.github.com>
2024-05-01 00:38:30 +0530

    Delete TIC-TAC-TOE directory

--- TIC-TAC-TOE/Eks-terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "githubaction-us-east-1" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- TIC-TAC-TOE/src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-const Button = ({ resetGame }) => {
-    return <button onClick={() => resetGame()}>New Game</button>;
-};
-
-export default Button;
